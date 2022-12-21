3b18075d5de616569ff13007b3d650ed84d6ccde
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-12-21 10:08:40 +0100

    read list and marker done todo

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const toDoList = [
+    {
+        text : 'fare la spesa',
+        done : false
+    },{
+        text : 'pulire la cucina',
+        done : false
+    },{
+        text : 'rinnovare passaporto',
+        done : true
+    },{
+        text : 'crostata per natale',
+        done : false
+    },{
+        text : 'comprare i regali',
+        done : true
+    },{
+        text : 'revisione auto',
+        done : true
+    },{
+        text : 'rinnovare abbonamento palestra',
+        done : false
+    }
+]
+
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+        return {
+            toDoList : toDoList
+        }
+    },
+    methods: {
+        
+    }
+}).mount('#app');
+
